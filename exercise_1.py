4f54dcf7a65b6baa16a9be1527d3b0797b6e45b8
Ferro Mauro <ferromauro@yahoo.it>
2020-07-19 10:34:06 +0200

    Exercise 1

---
+++ exercise_1.py
@@ -0,0 +1,37 @@
+# Example 1
+print("*"*13 + " Example 1: Input from Keyboard.  "+"*"*13)
+name = input("What's you name? \n")
+print(f"Hello {name}")
+print("*"*60)
+print("\n")
+
+# Example 2
+print("*"*13 + " Example 2: Input from Keyboard.  "+"*"*13)
+a = input("Variabile a: ")
+b = input("Variabile b: ")
+result = a+b
+print(f"a is a variable: {type(a)}")
+print(f"b is a variable: {type(b)}")
+print(f"The sum of 'a' and 'b' is: {result}")
+print("*"*60)
+print("\n")
+
+# Example 3
+print("*"*13 + " Example 3: Input from Keyboard.  "+"*"*13)
+a = int(input("Variabile a: "))
+b = int(input("Variabile b: "))
+result = a+b
+print(f"a is a variable: {type(a)}")
+print(f"b is a variable: {type(b)}")
+print(f"The sum of 'a' and 'b' is: {result}")
+print("*"*60)
+print("\n")
+
+
+# Task 1: write a program to read name and surname and print the sentence: "Nice to meet you {name} {surname}"
+
+
+
+
+# Task 2: Write a program to read variable a and variable b and print the result of a/b
+# Note you must put a and b like float types using float()
